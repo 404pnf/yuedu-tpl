e8fb1175dc5dd61fbb791eeef737da5b2db94c27
404pnf <404pnf@exmaple.org>
2014-08-11 11:29:56 +0800

    将jslint的警告从上百个减少到2个
    艰难，但不是不可能

--- public/js/user_ejs.js
+++ public/js/user_ejs.js
@@ -1,37 +1,48 @@
+
+/*jslint browser: true , nomen: true, indent: 2*/
+/*global $, jQuer, EJS, _ */
+
 // http://jslint.com/
+// http://www.jslint.com/lint.html
+// http://stackoverflow.com/questions/3039587/jslint-reports-unexpected-dangling-character-in-an-underscore-prefixed-variabl
+
+// jslint警告信息中文版
+// https://github.com/SFantasy/jslint-error-explanations-zh
 
 var YD = YD || {};
 
 
 (function () {
+  "use strict";
 
-  // work as status tracking accross YD.fn
-  YD.flags = YD.flags || {};
+  var showStatusMsg,
+    renderData,
+    postJson,
+    renderLocalData,
+    redirectToUrl;
 
   //
   // Utilities
   //
-  var showStatusMsg = function(data) {
+  showStatusMsg = function (data) {
     // 先清除之前的msg内容
     // http://api.jquery.com/empty/
-    $( '#msg' ).empty();
+    $('#msg').empty();
 
-    console.log( 'from showStatusMsg, showing each msg: ')
+    console.log('from showStatusMsg, showing each msg: ');
     _.each(data, function (v, k) {
       // http://api.jquery.com/append/
-      console.log( k + ': ' + v );
+      console.log(k + ': ' + v);
       $('#msg').append(['<div class=', k, '>', v, '</div>'].join(''));
-     });
-    };
+    });
+  };
 
-    // 下面两个函数在单独的模版文件中无法使用，报undefined
-    var getKey = function (o) { _.keys(o)[0] }
-      , getVal = function (o) { _.values(o)[0] }
-      // http://stackoverflow.com/questions/503093/how-can-i-make-a-redirect-page-in-jquery-javascript
-      // window.location.replace("http://stackoverflow.com");
-      , redirectToUrl = function(url) {
-        window.location.replace(url);
-      };
+
+  // http://stackoverflow.com/questions/503093/how-can-i-make-a-redirect-page-in-jquery-javascript
+  // window.location.replace("http://stackoverflow.com");
+  redirectToUrl = function (url) {
+    window.location.replace(url);
+  };
 
   //
   // renderData
@@ -45,22 +56,22 @@ var YD = YD || {};
   // @callback : a. massage json data if needed
   //             b. functions for side-effect, i.e, saving tmp data
   //             c. must explicitly return data or massaged data
-  var renderData = function (url, tpl, cssID, callback) {
+  renderData = function (url, tpl, cssID, callback) {
     $.get(url)
       .done(function (data) {
         //console.log('from renderData, showing callback function');
         //console.log(callback);
         //console.log(data, status, xhr)
-        var cb = callback || _.identity
+        var cb = callback || _.identity;
         new EJS({url: 'tpl/' + tpl}).update(cssID, cb(data));
       })
       .fail(function (data, status, xhr) {
         $('#msg').text(data, status, xhr).slideDown('slow');
-      })
-      .always(function (data, status, xhr) {
-        // both sucess and failure
       });
-    };
+      // .always(function (data, status, xhr) {
+      //   // both sucess and failure
+      // });
+  };
 
 
   // 1. get data from form
@@ -68,7 +79,7 @@ var YD = YD || {};
   // 3. post json to api
   // 4. error msg dipsplayed in html
   // for side-effect only
-  var postJson = function (url, cssID, callback) {
+  postJson = function (url, cssID, callback) {
     var form_data = $(cssID).serializeJSON();
     //console.log( 'from postJson, showing post data to ' + url + ': ');
     //console.log( form_data );
@@ -80,71 +91,80 @@ var YD = YD || {};
         showStatusMsg(data);
       })
       .fail(function (data) {
-        console.log( data );
+        console.log(data);
         $('#msg').text(data).slideDown('slow');
-      })
-      .always(function () {
-        // console.log( "postJson finished" );
       });
-    };
+      // .always(function () {
+      //   // console.log( "postJson finished" );
+      // });
+  };
+
+  renderLocalData = function (data, cssID, tpl, isVisable, callback) {
+    var cb = callback || _.identity,
+      show = isVisable;
+
+    //console.log('show show ' +  show)
+    if (show) {
+      new EJS({url: 'tpl/' + tpl}).update(cssID, cb(data));
+    }
+  };
 
 
   //
   // user.html 页面
   //
 
-  var renderLocalData = function (data, cssID, tpl, isVisable, callback) {
-    var cb = callback || _.identity;
-    var show = isVisable;
-    //console.log('show show ' +  show)
-    if (show) {
-      new EJS({url: 'tpl/' + tpl}).update(cssID, cb(data));
+  (function () {
+
+    var userPageInStack1 = _.partial(renderData, '/userController/show/loginUser', _, 'user_info', _),
+      userPageInStack2 = _.partial(renderData, '/userController/show/loginUser', _, 'user_photo', _);
+
+    YD.userShow = function () {
+      userPageInStack1('user_show.ejs', function (d) {
+        YD.userInfo = d;
+        return d;
+      });
     };
-  };
+
+    YD.userEdit = function () {
+      userPageInStack1('user_edit.ejs');
+    };
+
+    YD.userPhotoShow = function () {
+      userPageInStack2('user_photo.ejs');
+    };
+
+    YD.userPhotoEdit = function () {
+      renderData('/userController/photos', 'user_photo_edit.ejs', 'user_photo', function (d) {
+        var n = {};
+        n.photos = d;
+        return n;
+      });
+    };
+
+    YD.userSave = function () {
+      postJson('/userController/save', 'form#user_info', function (data) {
+        console.log('from YD.userSave, showing post data: ');
+        console.log(data);
+        if (data.success) {
+          YD.userShow();
+        }
+      });
+    };
+
+    YD.userPhotoSave = function () {
+      postJson('/userController/save', 'form#user_photo', function (data) {
+        // console.log( 'from YD.userPhotoSave, showing post data: ')
+        // console.log(data)
+        if (data.success) {
+          YD.userPhotoShow();
+        }
+      });
+    };
+
+  }());
 
   // 用 partial application 设定一些固定的参数
-  var userPageInStack1 = _.partial(renderData, '/userController/show/loginUser', _, 'user_info', _);
-  var userPageInStack2 = _.partial(renderData, '/userController/show/loginUser', _, 'user_photo', _);
-
-  YD.userShow = function () {
-    userPageInStack1('user_show.ejs', function(d) {
-      YD.userInfo = d;
-      return d;
-    });
-  };
-
-  YD.userEdit = function () { userPageInStack1('user_edit.ejs') };
-
-  YD.userPhotoShow = function () { userPageInStack2('user_photo.ejs') };
-
-  YD.userPhotoEdit = function () {
-    renderData('/userController/photos', 'user_photo_edit.ejs', 'user_photo', function(d) {
-      var n = {};
-      n['photos'] = d;
-      return n;
-    });
-  };
-
-  YD.userSave = function () {
-    postJson('/userController/save', 'form#user_info', function(data) {
-      console.log( 'from YD.userSave, showing post data: ')
-      console.log(data)
-      if (data.success) {
-        YD.userShow();
-      };
-    });
-  };
-
-  YD.userPhotoSave = function () {
-    postJson('/userController/save', 'form#user_photo', function(data) {
-      // console.log( 'from YD.userPhotoSave, showing post data: ')
-      // console.log(data)
-      if (data.success) {
-        YD.userPhotoShow();
-      };
-    });
-  };
-
 
 
   // start.html 生成页面的主函数
@@ -152,93 +172,102 @@ var YD = YD || {};
   YD.startDispache = function () {
     var repeat = function () {
       $.get('/examController/studentLogin')
-      .done(function (data) {
+        .done(function (data) {
 
-        // bind data to local variable
-        var examInfo = data;
+          // bind data to local variable
+          // predicts
+          // 判定时要注意，如果某objec他没有那个键名，我们去取值了，会报 Uncaught ReferenceError: latestExamResult is not defined
+          // 这是ejs报的错
+          var examInfo = data,
+            canTakeExam = !!examInfo.currentExam,
+            TookNoExam = canTakeExam && examInfo.currentExam.userExamState === '0',
+            hasUpcomingExam = !!examInfo.upcomingExam,
+            haslatestExamResult = !!examInfo.latestExamResult,
+            showExamSimulating = !haslatestExamResult,
+            startPageInStack1,
+            startPageInStack2,
+            examCurrent,
+            examSimulating,
+            examUpcoming,
+            examScores,
+            renderPage;
 
-        // predicts
-        // 判定时要注意，如果某objec他没有那个键名，我们去取值了，会报 Uncaught ReferenceError: latestExamResult is not defined
-        // 这是ejs报的错
-        var canTakeExam = !!examInfo.currentExam
-          , TookNoExam = canTakeExam && examInfo.currentExam.userExamState === '0'
-          , hasUpcomingExam = !!examInfo.upcomingExam
-          , haslatestExamResult = !!examInfo.latestExamResult
-          , showExamSimulating = !haslatestExamResult
+          // partial application to pre-configure functions
+          startPageInStack1 = _.partial(renderLocalData, examInfo, 'stack1');
+          startPageInStack2 = _.partial(renderLocalData, examInfo, 'stack2');
 
-        // partial application to pre-configure functions
-        var startPageInStack1 = _.partial(renderLocalData, examInfo, 'stack1')
-          , startPageInStack2 = _.partial(renderLocalData, examInfo, 'stack2')
-
-        // functions to render page
-        // 再测一次看看自己有没有进步？
-        // 你有测试尚未完成，可继续测试
-        // 你还没有测试。再来测一下
-        var examCurrent = function () {
-              startPageInStack2('start_current.ejs', canTakeExam, function (d) {
-                console.log(d);
-                var o = _.clone(d.currentExam);
-
-                o = _.extend(o, {button: '开始考试'});
-                console.log(o);
-                if (haslatestExamResult) {
-                  o = _.extend(o, {title: '再测一次看看自己有没有进步'});
-                  return o;
-                } else if (TookNoExam) {
-                  o = _.extend(o, {title: '你有测试尚未完成，可继续测试'});
-                  return o;
-                } else {
-                  o = _.extend(o, {title: '你还没有测试。再来测一下'});
-                  return o;
-                }
-              })
-            }
-          , examSimulating = function () {
-              startPageInStack1('start_simulating.ejs', showExamSimulating)
-            }
-          , examUpcoming = function () {
-              startPageInStack2('start_upcoming.ejs', hasUpcomingExam,  function (d) {
-                                upcomingExam = _.map(examInfo.upcomingExam, function (e) {
-                                  if (e.isTodayExam) {
-                                    e.endTime = '';
-                                    e.isTodayExam = '今天';
-                                    return e;
-                                  } else {
-                                    e.isTodayExam = '';
-                                    return e;
-                                  }
-                                });
-                                return upcomingExam;
-                              })
-            }
-          , examScores = function () {
-              startPageInStack1('start_scores.ejs', haslatestExamResult);
-            };
-
-        // main function
-        var renderPage = function () {
-          _.map([
-            examCurrent(),
-            examSimulating(),
-            examUpcoming(),
-            examScores()
-            ],
-            function (e) { e });
+          // functions to render page
+          // 再测一次看看自己有没有进步？
+          // 你有测试尚未完成，可继续测试
+          // 你还没有测试。再来测一下
+          examCurrent = function () {
+            startPageInStack2('start_current.ejs', canTakeExam, function (d) {
+              console.log(d);
+              var o = _.clone(d.currentExam);
+              o = _.extend(o, {button: '开始考试'});
+              console.log(o);
+              if (haslatestExamResult) {
+                o = _.extend(o, {title: '再测一次看看自己有没有进步'});
+              } else if (TookNoExam) {
+                o = _.extend(o, {title: '你有测试尚未完成，可继续测试'});
+              } else {
+                o = _.extend(o, {title: '你还没有测试。再来测一下'});
+              }
+              return o;
+            });
           };
 
-        // run repeat once to get data at once
-        // then run repeat every n millseconds
-        renderPage();
-      })
-      .fail(function (data, status, xhr) {
-        $('#msg').text(data, status, xhr).slideDown('slow');
-      });
+          examSimulating = function () {
+            startPageInStack1('start_simulating.ejs', showExamSimulating);
+          };
+
+          examUpcoming = function () {
+            startPageInStack2('start_upcoming.ejs', hasUpcomingExam,  function () {
+              var upcomingExam = _.map(examInfo.upcomingExam, function (e) {
+                if (e.isTodayExam) {
+                  e.endTime = '';
+                  e.isTodayExam = '今天';
+                } else {
+                  e.isTodayExam = '';
+                }
+                //return e;
+              });
+              return upcomingExam;
+            });
+          };
+
+          examScores = function () {
+            startPageInStack1('start_scores.ejs', haslatestExamResult);
+          };
+
+          // main function
+          renderPage = function () {
+            _.map(
+              [
+                examCurrent(),
+                examSimulating(),
+                examUpcoming(),
+                examScores()
+              ],
+              function (e) {
+                e();
+              }
+            );
+          };
+
+          // run repeat once to get data at once
+          // then run repeat every n millseconds
+          renderPage();
+        })
+        .fail(function (data, status, xhr) {
+          $('#msg').text(data, status, xhr).slideDown('slow');
+        });
     };
 
-    return function () {
+    return (function () {
       repeat();
       setInterval(repeat, 2000);
-    }();
+    }());
   };
 
 }()); // end of let scope
