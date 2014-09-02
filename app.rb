e258d1c16e52c6210cc7d9e42306e432c50c67c2
404pnf <404pnf@exmaple.org>
2014-09-02 16:10:40 +0800

    将不使用的文件放到tmp文件夹中

--- app.rb
+++ app.rb
@@ -13,12 +13,10 @@ end
 
 get '/' do
   text = %q(
-    <a href=/user.html>user.html</a><br>
-    <a href=/start.html>start.html</a><br>
-    <a href=/login.html>login.html</a><br>
     <a href=/chart.html>chart.html</a><br>
     <a href=/personal_info.html>personal_info.html</a><br>
     <a href=/front.html>front.html</a><br>
+    <a href=/login.html>login.html</a><br>
   )
   text
 end
