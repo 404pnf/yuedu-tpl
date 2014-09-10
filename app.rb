77ba51dec83fc6cfbbf8593f0c27e4758144ee76
404pnf <404pnf@exmaple.org>
2014-09-10 17:23:41 +0800

    头像编辑也完成了。等待调整样式，因为把提交按钮从a标签给位了button

--- app.rb
+++ app.rb
@@ -24,13 +24,13 @@ end
 get '/userController/photos' do
   photos = {
     photos: [
-      { key: 'a', value: '/images/head.png' },
-      { key: 'b', value: '/images/head.png' },
-      { key: 'c', value: '/images/head.png' },
-      { key: 'd', value: '/images/head.png' },
-      { key: 'e', value: '/images/head.png' },
-      { key: 'f', value: '/images/head.png' },
-      { key: 'g', value: '/images/head.png' }
+      { key: 'a', value: 'images/head1.png'  },
+      { key: 'b', value: 'images/head1.png'  },
+      { key: 'c', value: 'images/head1.png'  },
+      { key: 'd', value: 'images/head1.png'  },
+      { key: 'e', value: 'images/head1.png'  },
+      { key: 'f', value: 'images/head1.png'  },
+      { key: 'g', value: 'images/head1.png'  }
     ]
   }
   JSONP photos
@@ -54,7 +54,7 @@ get '/userController/show/loginUser' do
     gender: { key: 'male', value: '男' },
     age: 12,
     dob: '2004-07-26 11:37:53 +0800',
-    photo: { key: 'e', value: '/images/head.png' },
+    photo: { key: 'e', value: 'images/head1.png' },
     grade: { key: 'd', value: '小学四年级' },
     school: '史家胡同小学',
     email: 'ererer@fltrp.com',
