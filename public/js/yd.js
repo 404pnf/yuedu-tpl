70c77b8222ede15e2f28ac27ca612e6319b793a4
404pnf <404pnf@exmaple.org>
2014-09-15 09:22:12 +0800

    不再使用jquery validation手写登陆前台验证

--- public/js/yd.js
+++ public/js/yd.js
@@ -356,68 +356,6 @@ YD = YD || {};
   // ## 登陆页面
   //
   YD.userLogin = function () {
-    // highlight
-    // var elements = $("input[type!='submit'], textarea, select");
-
-    // elements.focus(function () {
-    //   $(this).parents("li").addClass("highlight");
-    // });
-    // elements.blur(function () {
-    //   $(this).parents("li").removeClass("highlight");
-    // });
-
-    // $("#forgotpassword").click(function () {
-    //   $("#password").removeClass("required");
-    //   $("#login").submit();
-    //   $("#password").addClass("required");
-    //   return false;
-    // });
-
-    // $("#login").validate();
-
-    //
-    // 修改该 jquery validation 插件的报错信息到中文
-    //
-    // 如需要修改，可在js代码中加入：
-    // http://www.open-open.com/lib/view/open1342179346214.html
-    //
-    // jQuery.extend(jQuery.validator.messages, {
-    //   required: "必填字段",
-    //   remote: "请修正该字段",
-    //   email: "请输入正确格式的电子邮件",
-    //   url: "请输入合法的网址",
-    //   date: "请输入合法的日期",
-    //   dateISO: "请输入合法的日期 (ISO).",
-    //   number: "请输入合法的数字",
-    //   digits: "只能输入整数",
-    //   creditcard: "请输入合法的信用卡号",
-    //   equalTo: "请再次输入相同的值",
-    //   accept: "请输入拥有合法后缀名的字符串",
-    //   maxlength: jQuery.validator.format("请输入一个长度最多是 {0} 的字符串"),
-    //   minlength: jQuery.validator.format("请输入一个长度最少是 {0} 的字符串"),
-    //   rangelength: jQuery.validator.format("请输入一个长度介于 {0} 和 {1} 之间的字符串"),
-    //   range: jQuery.validator.format("请输入一个介于 {0} 和 {1} 之间的值"),
-    //   max: jQuery.validator.format("请输入一个最大为 {0} 的值"),
-    //   min: jQuery.validator.format("请输入一个最小为 {0} 的值")
-    // });
-
-    // ## 注意
-    // 直接在html中用 method='post'提交了
-    // 由后台来处理登陆成功后去哪里
-    // 由后台来处理登陆失败去哪里和显示什么
-
-
-    //
-    // return (function () {
-    //   // 提交表单
-    //   $("form").submit(function (e) {
-    //     e.preventDefault();
-    //     postJson("/userController/login", "form#login", function () {
-    //       wrap(redirectToUrl("/front.html"));
-    //     });
-    //   });
-    // }());
-
     // NB: in JS, empty string is false
     // '' is false
     // ' ' with a space is true
