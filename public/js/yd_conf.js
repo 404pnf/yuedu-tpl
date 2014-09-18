3027d58e2163dadfff61c46acc4674b51cc7d7f2
404pnf <404pnf@exmaple.org>
2014-09-18 13:40:52 +0800

    增加查看考试报告的url到配置文件

--- public/js/yd_conf.js
+++ public/js/yd_conf.js
@@ -4,17 +4,19 @@ YD = {};
 
 YD.conf = {
   tplDir: "/tpl/",
-  siteHomeUrl: "/front.html",
-  simulationExam: "/simulation_exam.html",
+  // siteHomeUrl: "/front.html",
+  // simulationExam: "/simulation_exam.html",
   userLogout: "/log/out ",
-  pwEditUrl: "/pw_edit.html",
+  // pwEditUrl: "/pw_edit.html",
   userHomeUrl: "/personal_info.html",
   userinfo: "/userController/show/loginUser",
+  userTrend: "",
   grades: "/userController/grades",
   photos: "/userController/photos",
   getExamInfo: "/examController/studentLogin",
   userSave: "/userController/save",
   userLogin: "/userController/login",
-  userResetPass: "/students/password/reset"
+  userResetPass: "/students/password/reset",
+  lastExamResult: "/test/personalReport/"
 };
 
