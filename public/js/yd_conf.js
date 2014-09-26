f1943a3b87ac3c4b356a967710899641c6a76377
404pnf <404pnf@exmaple.org>
2014-09-26 17:29:12 +0800

    增加修改密码的api路径到配置文件

--- public/js/yd_conf.js
+++ public/js/yd_conf.js
@@ -20,7 +20,8 @@
     photos: '/userController/photos',
     getExamInfo: '/examController/studentLogin',
     userSave: '/userController/save',
-    userLogin: '/userController/login'
+    userLogin: '/userController/login',
+    userResetPass: '/students/password/reset'
   };
 
 }).call(this);
